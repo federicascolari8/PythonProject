c7b157b13d1ea18a89a2459a4704cea771ecd42e
beatriznegreiros <beatriznegreiros@outlook.com>
2022-01-22 19:38:53 +0100

    app can load ex. files and process stat. ana.

--- web_application.py
+++ web_application.py
@@ -1,106 +1,166 @@
+import base64
+import datetime
+import io
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import plotly.express as px
+import openpyxl
+from statistical_analyzer import *
+from utils import *
+
 import pandas as pd
-import plotly.express as px  # (version 4.7.0 or higher)
-import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+from config import *
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# TODO
-# -- Import and clean data (importing csv into pandas)
-df = pd.read_csv("overview_freezecores.csv")
-# df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
-# df.reset_index(inplace=True)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
+                suppress_callback_exceptions=True)
 
-
-# ------------------------------------------------------------------------------
-# App layout
-app.layout = html.Div([
-
-    html.H1("Sediment Analyst", style={'text-align': 'center'}),
-
-    # TODO: upload files
-    # dcc.Upload(
-    #     id='upload-data',
-    #     children=html.Div([
-    #         'Drag and Drop or ',
-    #         html.A('Select Files')
-    #     ]),
-    #     style={
-    #         'width': '100%',
-    #         'height': '60px',
-    #         'lineHeight': '60px',
-    #         'borderWidth': '1px',
-    #         'borderStyle': 'dashed',
-    #         'borderRadius': '5px',
-    #         'textAlign': 'center',
-    #         'margin': '10px'
-    #     },
-
-    # TODO: isolate time components and implement time slider
-    # dcc.RangeSlider(
-    #     min=0,
-    #     max=10,
-    #     step=None,
-    #     marks={
-    #         0: '0°F',
-    #         3: '3°F',
-    #         5: '5°F',
-    #         7.65: '7.65°F',
-    #         10: '10°F'
-    #     },
-    #     value=[3, 7.65]
-    # ),
-
-    # TODO: isolate samples
-    # dcc.Dropdown(id="slct_timeobjct",
-    #              options=[
-    #                  {"label": "", "value": 2015}, # here we need to filter samples according to the time component (see above)
-    #                  {"label": "2016", "value": 2016},
-    #                  {"label": "2017", "value": 2017},
-    #                  {"label": "2018", "value": 2018}],
-    #              multi=False,
-    #              value=None,
-    #              style={'width': "40%"}
-    #              ),
-
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-
-    dcc.Graph(id='cum_gsd', figure={})
-
+app.layout = html.Div([ # this code section taken from Dash docs https://dash.plotly.com/dash-core-components/upload
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-div'),
+    html.Div(id='output-datatable'),
 ])
 
 
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='cum_gsd', component_property='figure')],
-    [Input(component_id='slct_timeobjct', component_property='value')]
-)
-def update_graph(slct_timeobjct, slct_sample):
-    container = "The time chosen by user was: {}".format(slct_timeobjct)
-    dff = df.copy()  # always a good idea to copy the dataframe to ensure
-    dff = dff[dff["Time Option"] == slct_timeobjct]
-    # dff = dff[dff["Sample"] == slct_sample]
+def parse_contents(contents, filename, date, df_global):
+    content_type, content_string = contents.split(',')
 
-    # Plotly Express
-    fig = px.scatter_mapbox(dff,
-                            hover_name='Sample name',
-                            hover_data=['d50', 'dm'], # for example
-                            lat='lat',
-                            lon='lon',
-                            zoom=3,
-                            height=300,
-                            color_discrete_sequence=['green'])
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded), engine="openpyxl", header=None)
 
-    fig.update_layout(mapbox_style='open-street-maps')
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    fig.show()
+        # clean dataset
+        dff = df.copy()
+        columns_to_get = [input["gs_clm"], input["cw_clm"]]
+        dff_gs = dff.iloc[input["header"]: input["header"] + input["n_rows"], columns_to_get]
+        dff_gs.reset_index(inplace=True, drop=True)
+        dff_gs = dff_gs.astype(float)
+        # Get metadata from the dataframe
+        # get sample name
+        try:
+            samplename = dff.iat[input["index_sample_name"][0], input["index_sample_name"][1]]
+        except :
+            samplename = None
+            pass
 
-    return container, fig
+        # get sample date
+        try:
+            sampledate = dff.iat[input["index_sample_date"][0], input["index_sample_date"][1]]
+        except:
+            sampledate = None
+            pass
+
+        # get sample coordinates
+        try:
+            lat = dff.iat[input["index_lat"][0], input["index_lat"][1]]
+            long = dff.iat[input["index_long"][0], input["index_long"][1]]
+        except:
+            lat, long = None, None
+            pass
+
+        metadata = [samplename, sampledate, (lat, long)]
+
+        # Rename and standardize the Grain Size dataframe
+        dff_gs.rename(columns={dff_gs.columns[0]: "Grain Sizes [mm]", dff_gs.columns[1]: "Fraction Mass [g]"},
+                      inplace=True)
+
+        analyzer = StatisticalAnalyzer(sieving_df=dff_gs, metadata=metadata)
+        df_global = append_global(obj=analyzer,
+                                  df=df_global
+                                  )
+
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing the file. Ensure that your file does not contain too many columns (< 15).'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        # html.H6(datetime.datetime.fromtimestamp(date)),
+        html.P("File successfully read"),
+        # dcc.Dropdown(id='xaxis-data',
+        #              options=[{'label':x, 'value':x} for x in df.columns]),
+        # html.P("Inset Y axis data"),
+        # dcc.Dropdown(id='yaxis-data',
+        #              options=[{'label':x, 'value':x} for x in df.columns]),
+        # html.Button(id="submit-button", children="Create Graph"),
+        html.Hr(),
+
+        #  Print data table of the grain sizes and class weights:
+        dash_table.DataTable(
+            data=dff_gs.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in dff_gs.columns],
+            page_size=30
+        ),
+        dcc.Store(id='stored-data', data=dff_gs.to_dict('records')),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
 
 
-# ------------------------------------------------------------------------------
+@app.callback(Output('output-datatable', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        df_global = pd.DataFrame()
+        children = [
+            parse_contents(c, n, d, df_global) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
+@app.callback(Output('output-div', 'children'),
+              Input('submit-button','n_clicks'),
+              State('stored-data','data'),
+              State('xaxis-data','value'),
+              State('yaxis-data', 'value'))
+def make_graphs(n, data, x_data, y_data):
+    if n is None:
+        return dash.no_update
+    else:
+        bar_fig = px.bar(data, x=x_data, y=y_data)
+        # print(data)
+        return dcc.Graph(figure=bar_fig)
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
